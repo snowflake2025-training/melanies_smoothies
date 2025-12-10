e03d84ec55070c729dcb5153d6bec6c610d84b73
snowflake2025-training <dj_ks@hotmail.co.uk>
2025-12-10 10:48:58 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect ('Choose up to 5 ingredients:', my_dataframe,max_selections=5)
 
 if ingredients_list:
-
     ingredients_string=''
-
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
