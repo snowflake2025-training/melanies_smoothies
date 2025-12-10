8f7d48510e63dba0e8b94ff9c9923461b80b9a99
snowflake2025-training <dj_ks@hotmail.co.uk>
2025-12-10 10:45:23 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,11 +29,10 @@ if ingredients_list:
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width =True)
         st.write(ingredients_string); 
-
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','""" + name_on_order+"""')"""
-   st.write(my_insert_stmt)
-   time_to_insert = st.button('Submit Order')
+      
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) values ('""" + ingredients_string + """','""" + name_on_order+"""')"""
+        st.write(my_insert_stmt)
+        time_to_insert = st.button('Submit Order')
 
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
