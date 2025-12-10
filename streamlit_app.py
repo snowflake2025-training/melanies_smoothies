cf3088cced23e183f58d9c9bf0dc94948c6012c7
snowflake2025-training <dj_ks@hotmail.co.uk>
2025-12-10 11:43:56 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,10 @@ ingredients_list = st.multiselect ('Choose up to 5 ingredients:', my_dataframe,m
 
 if ingredients_list:
     ingredients_string=''
+
+  search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+  st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
